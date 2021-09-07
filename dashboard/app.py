b3aa0dc46b7ab2bbc1c73bc39866a09e0e2faac5
Jocelyne Vetillard <jocelyne.vetillard@wanadoo.fr>
2021-09-07 11:43:53 +0200

    ajout de fichiers pour le déploiement, corrections de bugs

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,11 +1,10 @@
 import pickle
 import pandas as pd
 import numpy as np
-import plotly.graph_objects as go
 import matplotlib.pyplot as plt
 import streamlit as st
 import math
-
+import sklearn
 
 ######################################
 # recupération des données 
